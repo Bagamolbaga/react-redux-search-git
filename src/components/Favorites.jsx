a165b701d011e73cd53e1a4770098f49902e7ee9
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-05-26 15:55:27 +0400

    Итоговый функционал без redux

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,10 +3,10 @@ import MyTable from './Table'
 
 const Favorites = ({data}) => {
   return (
-    <div>
+    <>
       <h2 className='favorite-tittle'>Favorites</h2>
       {data.length !== 0 ? <MyTable data={data} favorites/> : <p>У вас нет избранных</p>}
-    </div>
+    </>
   )
 }
 
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -7,13 +7,15 @@ import Favorites from '../components/Favorites'
 
 const Result = () => {
   let {data, favorites, isLoading, pages, btnPageHandler, currentPage} = useContext(Context)
- 
+  
+
   return (
     <>
         <Search />
         
         {isLoading ? <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> : <MyTable data={data}/>}
-        {data.length !== 0 && pages.map((page, i) => (<button className={currentPage === page ? 'btn-page_current' : 'btn-page'} onClick={() => btnPageHandler(page)} key={i}>{page}</button>) )}
+        
+        {!isLoading & data.length !== 0 ? pages.map((page, i) => (<button className={currentPage === page ? 'btn-page_current' : 'btn-page'} onClick={() => btnPageHandler(page)} key={i}>{page}</button>)): ''}
     
         <Favorites data={favorites}/>
     </>
